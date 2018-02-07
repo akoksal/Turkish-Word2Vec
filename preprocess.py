349ee1e79583ce90ec229e1b5cf8e24b1242619e
akoksal <abdullatifkoksal@gmail.com>
2018-02-07 15:20:34 +0300

    Fixes #1

--- preprocess.py
+++ preprocess.py
@@ -8,16 +8,17 @@ import logging
 import string
 from gensim import utils
 
+
+def tokenize_tr(content,token_min_len=2,token_max_len=50,lower=True):
+	if lower:
+		lowerMap = {ord(u'A'): u'a',ord(u'A'): u'a',ord(u'B'): u'b',ord(u'C'): u'c',ord(u'Ç'): u'ç',ord(u'D'): u'd',ord(u'E'): u'e',ord(u'F'): u'f',ord(u'G'): u'g',ord(u'Ğ'): u'ğ',ord(u'H'): u'h',ord(u'I'): u'ı',ord(u'İ'): u'i',ord(u'J'): u'j',ord(u'K'): u'k',ord(u'L'): u'l',ord(u'M'): u'm',ord(u'N'): u'n',ord(u'O'): u'o',ord(u'Ö'): u'ö',ord(u'P'): u'p',ord(u'R'): u'r',ord(u'S'): u's',ord(u'Ş'): u'ş',ord(u'T'): u't',ord(u'U'): u'u',ord(u'Ü'): u'ü',ord(u'V'): u'v',ord(u'Y'): u'y',ord(u'Z'): u'z'}
+		content = content.translate(lowerMap)
+	return [
+	utils.to_unicode(token) for token in utils.tokenize(content, lower=False, errors='ignore')
+	if token_min_len <= len(token) <= token_max_len and not token.startswith('_')
+	]
+
 if __name__ == '__main__':
-	
-	def tokenize_tr(content,token_min_len=2,token_max_len=50,lower=True):
-		if lower:
-			lowerMap = {ord(u'A'): u'a',ord(u'A'): u'a',ord(u'B'): u'b',ord(u'C'): u'c',ord(u'Ç'): u'ç',ord(u'D'): u'd',ord(u'E'): u'e',ord(u'F'): u'f',ord(u'G'): u'g',ord(u'Ğ'): u'ğ',ord(u'H'): u'h',ord(u'I'): u'ı',ord(u'İ'): u'i',ord(u'J'): u'j',ord(u'K'): u'k',ord(u'L'): u'l',ord(u'M'): u'm',ord(u'N'): u'n',ord(u'O'): u'o',ord(u'Ö'): u'ö',ord(u'P'): u'p',ord(u'R'): u'r',ord(u'S'): u's',ord(u'Ş'): u'ş',ord(u'T'): u't',ord(u'U'): u'u',ord(u'Ü'): u'ü',ord(u'V'): u'v',ord(u'Y'): u'y',ord(u'Z'): u'z'}
-			content = content.translate(lowerMap)
-		return [
-        utils.to_unicode(token) for token in utils.tokenize(content, lower=False, errors='ignore')
-        if token_min_len <= len(token) <= token_max_len and not token.startswith('_')
-		]
 
 	if len(sys.argv) < 3:
 		print("Please provide two arguments, first one is path to the wikipedia dump, second one is path to the output file")
