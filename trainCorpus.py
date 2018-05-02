ac4be88323ed6c6c06f5b330fff2921ea6f7e313
akoksal <abdullatifkoksal@gmail.com>
2018-05-02 15:21:05 +0300

    Gensim is updated to latest version, output vectors file is changed to a binary file.

--- trainCorpus.py
+++ trainCorpus.py
@@ -9,7 +9,7 @@ from gensim.models.word2vec import LineSentence
 if __name__ == '__main__':
 
 	if len(sys.argv) < 3:
-		print("Please provide two arguments, first one is path to the revised corpus, second one is path to the output file for model")
+		print("Please provide two arguments, first one is path to the revised corpus, second one is path to the output file for model.")
 		print("Example command: python3 word2vec.py wiki.tr.txt trmodel")
 		sys.exit()
 
@@ -21,4 +21,4 @@ if __name__ == '__main__':
                     format='%(asctime)s %(levelname)s %(message)s')
 
 	model = Word2Vec(LineSentence(inputFile), size=400, window=5, min_count=5, workers=multiprocessing.cpu_count())
-	model.save(outputFile)
+	model.wv.save_word2vec_format(outputFile, binary=True)
